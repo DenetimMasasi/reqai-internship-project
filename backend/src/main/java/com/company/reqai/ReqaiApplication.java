a92e2e0ec7b7cc2c0c9719e9feeb89ea524e0722
DenetimMasasi <113472009+DenetimMasasi@users.noreply.github.com>
2026-07-13 18:13:58 +0300

    Configure PostgreSQL database connection

--- backend/src/main/java/com/company/reqai/ReqaiApplication.java
+++ backend/src/main/java/com/company/reqai/ReqaiApplication.java
@@ -2,13 +2,8 @@ package com.company.reqai;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.hibernate.autoconfigure.HibernateJpaAutoConfiguration;
-import org.springframework.boot.jdbc.autoconfigure.DataSourceAutoConfiguration;
 
-@SpringBootApplication(exclude = {
-    DataSourceAutoConfiguration.class,
-    HibernateJpaAutoConfiguration.class
-})
+@SpringBootApplication
 public class ReqaiApplication {
 
     public static void main(String[] args) {
